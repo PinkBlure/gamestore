8fced4e859e5284c19226c712717f90b0f39baf1
Aileen <aileenpadrontorres511@.com>
2024-10-24 09:50:35 +0100

    Footer

--- src/detalle.php
+++ src/detalle.php
@@ -4,10 +4,32 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Juego</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
+
+  <nav class="navbar navbar-expand-lg" style="background-color: purple;">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#">GameStore</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="./src/admin.php">Administrar Tienda Online</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="./src/juegos.php">Ver lista de juegos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <main class="container text-center my-4 flex-grow-1">
   <?php
   if (!isset($_GET['id'])) {
     echo "<script>console.log(Error: 'No existe el producto con este ID')</script>";
@@ -44,6 +66,16 @@
 
   $conn = null;
   ?>
+  </main>
+
+  <footer class="bg-purple text-white text-center text-lg-start mt-auto" style="background-color: purple;">
+    <div class="text-center p-3">
+      © 2024 GameStore - Todos los derechos reservados.
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
